0e431c0820becec1d390a589334baee705844a16
Ashley Archibald <aarchibald20@gmail.com>
2019-02-18 16:19:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_nyc_pigeon = {}
+
+  data.each do |attribute, attributes|
+    newdata.each do |key,value|
+      value.each do |name|
+        new_nyc_pigeon[name] = {:color => [],
+        :gender => [], :lives => []}
+      end
+    end
 end
